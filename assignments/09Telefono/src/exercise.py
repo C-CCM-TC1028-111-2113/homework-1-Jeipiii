9ce047c146e713dceaf6b56ceb8bf28a93bde8a7
Jeipiii <88851076+Jeipiii@users.noreply.github.com>
2021-08-26 13:17:06 -0500

    Update exercise.py

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,9 +1,9 @@
 costo = float(0.80)
 
 
-numero_de_mensajes = int(input("Dame el número de mesnajes"))
-numero_de_megas = float(input("Dame el número de mesnajes"))
-numero_de_minutos = float(input("Dame el número de mesnajes"))
+numero_de_mensajes = int(input("Dame el número de mensajes"))
+numero_de_megas = float(input("Dame el número de mensajes"))
+numero_de_minutos = float(input("Dame el número de mensajes"))
 
 costo_mensual = costo *(numero_de_mensajes + numero_de_megas + numero_de_minutos)
 print(" El costo mensual por servicios fue : " + str(costo_mensual))
