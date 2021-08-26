7d3fc27acefd3d46cce2ad84f258bb1685bcb81f
Jeipiii <88851076+Jeipiii@users.noreply.github.com>
2021-08-26 14:32:38 -0500

    Update exercise.py

--- assignments/07PendienteLinea/src/exercise.py
+++ assignments/07PendienteLinea/src/exercise.py
@@ -1,8 +1,9 @@
-def main():
-    #escribe tu código abajo de esta línea
-    #Lee los datos
-    pass
+x1 =int(input("Dame el valor de x1 : " ))
+x2 =int(input("Dame el valor de x2 : " ))
+y1 =int(input("Dame el valor de y1 : " ))
+y2 =int(input("Dame el valor de y2 : " ))
 
+m = (y2-y1)/(x2-x1)
 
-if __name__ == '__main__':
-    main()
+print(m)
+
