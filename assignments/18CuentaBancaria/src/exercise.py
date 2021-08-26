5d84b6d24861c80872deed43a3e069640834e216
Jeipiii <88851076+Jeipiii@users.noreply.github.com>
2021-08-26 13:48:33 -0500

    Update exercise.py

--- assignments/18CuentaBancaria/src/exercise.py
+++ assignments/18CuentaBancaria/src/exercise.py
@@ -1,5 +1,18 @@
 def main():
     #escribe tu código abajo de esta línea
+    saldo_prev_mes = float(input(" Defina el saldo del mes anterior : "))
+    ingresos = float(input("Defina su saldo "))
+    egresos = float(input(" Defina sus egresos"))
+    n_chques = int(input("Cuantos chuques tienes ? : "))
+    costo_chques = (n_chques + 13)
+
+    total = (saldo_prev_mes + ingresos + egresos + costo_chques)* 0.075
+
+
+    print("su total es : " + str(total))
+    
+    
+    
     pass
 
 if __name__ == '__main__':
